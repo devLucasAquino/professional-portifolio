23b867f295b480e167e4a474828243661bb4302e
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-03-06 17:45:22 -0300

    start portifolio part

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,14 @@
-// just commit
+// Seletor para o contêiner do carousel
+const carouselContainer = document.querySelector('carrossel-container');
+// Largura de um item no carousel
+const itemWidth = document.querySelector('.carrossel-item').clientWidth;
+
+// Função para mover o carousel
+function moveCarousel(direction) {
+  const currentPosition = carouselContainer.style.transform ? parseInt(carouselContainer.style.transform.match(/-?\d+/)[0]) : 0;
+  const newPosition = direction === 'next' ? currentPosition - itemWidth : currentPosition + itemWidth;
+  carouselContainer.style.transform = `translateX(${newPosition}px)`;
+}
+
+document.getElementById('prevBtn').addEventListener('click', () => moveCarousel('prev'));
+document.getElementById('nextBtn').addEventListener('click', () => moveCarousel('next'));
